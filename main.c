94359abb33e02dfa1717a5fcdcc461463bc7207a
Guicha <48026427+Guicha@users.noreply.github.com>
2023-11-13 18:20:07 +0100

    Petites modifs

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main() {
 
     printf("Projet d'algo");
 
-    printf("\nVous devez voir ce message uniquement si vous etes sur la branche guy-charbel. Sinon, je n'existe pas !");
+    printf("\nVous devez voir ce message uniquement si vous etes sur la branche guy-charbel. Sinon, je n'existe pas ! :)");
 
     printf("\n");
     system("pause");
