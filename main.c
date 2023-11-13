2277f9840c76b25d7da5c812bc7a8c8949d3ee00
Guicha <48026427+Guicha@users.noreply.github.com>
2023-11-13 18:36:43 +0100

    Tests

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    printf("Projet d'algo");
+    printf("Projet d'algo :: Si vous voyez ce message, c'est que la branche main a bien ete merge sur votre brancher perso !");
 
     printf("\n");
     system("pause");
