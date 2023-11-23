c99700842594334c11e460426f91218b165ef5e5
yanisdenizot <yanis.denizot@edu.ece.fr>
2023-11-23 18:54:31 +0000

    commentaire des sous-programmes de l'exclusion

--- Exclusion/est_compatible_avec_station_exclusion.c
+++ Exclusion/est_compatible_avec_station_exclusion.c
@@ -2,15 +2,24 @@
 // Created by Yanis Denizot on 23/11/2023.
 //
 #include "../header.h"
+// Définition de la fonction est_compatible_avec_station_exclusion
+// Cette fonction vérifie si une opération spécifique ('operation') peut être ajoutée à une station donnée ('station')
+// sans violer les contraintes d'exclusion spécifiées dans le graphe ('graphe').
 int est_compatible_avec_station_exclusion(t_graphe *graphe, t_station *station, int operation) {
+    // Boucle sur chaque opération déjà présente dans la station
     for (int i = 0; i < station->taille; i++) {
+        // Récupère l'opération actuelle de la station
         int operation_station = station->operations[i];
+
+        // Boucle sur chaque arête (contrainte d'exclusion) du graphe
         for (int j = 0; j < graphe->taille; j++) {
+            // Vérifie si l'opération à ajouter est en conflit avec l'opération actuelle de la station.
+            // Un conflit se produit si une paire d'exclusion entre ces deux opérations existe dans le graphe.
             if ((graphe->tab_aretes[j].sommet1 == operation && graphe->tab_aretes[j].sommet2 == operation_station) ||
                 (graphe->tab_aretes[j].sommet2 == operation && graphe->tab_aretes[j].sommet1 == operation_station)) {
-                return 0; // Non compatible
+                return 0; // Retourne 0 si un conflit est trouvé, indiquant que l'opération n'est pas compatible.
             }
         }
     }
-    return 1; // Compatible
+    return 1; // Retourne 1 si aucune incompatibilité n'est trouvée, indiquant que l'opération est compatible.
 }
--- Exclusion/est_dans_le_tableau_exclusion.c
+++ Exclusion/est_dans_le_tableau_exclusion.c
@@ -2,12 +2,17 @@
 // Created by Yanis Denizot on 23/11/2023.
 //
 #include "../header.h"
-// Vérifie si un élément est déjà dans le tableau
+
+// Définition de la fonction est_dans_le_tableau_exclusion
+// Cette fonction vérifie si un élément spécifique ('element') est présent dans un tableau donné ('tableau').
+// Elle prend également en entrée la taille du tableau ('taille'), qui est le nombre d'éléments dans le tableau.
 int est_dans_le_tableau_exclusion(int *tableau, int taille, int element) {
+    // Boucle sur chaque élément du tableau
     for (int i = 0; i < taille; i++) {
+        // Vérifie si l'élément actuel du tableau est égal à l'élément recherché
         if (tableau[i] == element) {
-            return 1; // L'élément est trouvé dans le tableau
+            return 1; // Si l'élément est trouvé, retourne 1 (vrai)
         }
     }
-    return 0; // L'élément n'est pas dans le tableau
+    return 0; // Si l'élément n'est pas trouvé après avoir parcouru tout le tableau, retourne 0 (faux)
 }
