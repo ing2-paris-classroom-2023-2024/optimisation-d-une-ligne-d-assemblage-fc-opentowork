25f0dc56c83817121b8ca260ab2e412080f52f24
Guicha <48026427+Guicha@users.noreply.github.com>
2023-11-26 18:17:44 +0100

    Retrait des commentaires du main.c

--- main.c
+++ main.c
@@ -2,32 +2,6 @@
 
 int main() {
 
-    printf("Projet d'algo :: Branche main ! \n");
-
-    /// Tests de guicha (vous pouvez décommenter si vous voulez tester)
-    /*t_graphe * liste_precedences;
-
-    liste_precedences = lire_fichier("precedences/temps");
-
-    printf("=== PRECEDENCES === \n");
-    for (int i=0; i<liste_precedences->taille; i++) {
-
-        printf("Paire %d: %d %d \n", i, liste_precedences->tab_aretes[i].sommet1, liste_precedences->tab_aretes[i].sommet2);
-    }
-
-    printf("\n\n");
-
-    printf("=== OPERATIONS === \n");
-    for (int i=0; i<liste_precedences->ordre; i++) {
-
-        printf("Paire %d: %d %f \n", i, liste_precedences->tab_sommets[i].numero, liste_precedences->tab_sommets[i].temps_execution);
-    }
-
-    printf("\n\n");
-
-    printf("=== TEMPS DE CYCLE === \n");
-    printf("Valeur: %d", liste_precedences->temps_cycle);*/
-
     printf("\n");
     system("pause");
 
