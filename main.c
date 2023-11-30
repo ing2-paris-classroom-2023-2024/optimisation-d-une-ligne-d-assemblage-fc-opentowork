b72040d82bbdd5bb848843b8370f2ca7c77f2ccb
adam2504 <adam.jouini@edu.ece.fr>
2023-11-30 14:23:50 +0100

    TEST

--- main.c
+++ main.c
@@ -5,18 +5,22 @@ int main() {
     printf("Projet d'algo :: Branche Adam \n");
 
     /// Tests de guicha (vous pouvez décommenter si vous voulez tester)
-    /*t_graphe * liste_exclusions;
+    /*
+    t_graphe * liste_exclusions;
 
     liste_exclusions = lire_fichier("exclusions");
 
-    for (int i=0; i<liste_exclusions->taille; i++) {
-
+    for (int i=0; i<liste_exclusions->taille; i++)
+    {
         printf("Paire %d: %d %d \n", i, liste_exclusions->tab_aretes[i].sommet1, liste_exclusions->tab_aretes[i].sommet2);
-    }*/
+    }
 
-    printf("Ceci est un nouveau test/n")
+    */
+
+    printf("Ceci est un nouveau test\n");
 
     printf("\n");
+
     system("pause");
 
     return 0;
